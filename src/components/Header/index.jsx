68908a5af06bb2f04002a6922f1b445e534ac92f
YuriCampos <camposyuri0@gmail.com>
2026-01-27 22:05:43 -0300

    ajustes

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,64 @@
-import { HeaderContainer } from './styles';
+import { HeaderContainer, HeaderDiv } from './styles';
 import logo from "../../assets/logo.png";
+import helpIcon from "../../assets/Help circle.png";
+import WhatsappIcon from "../../assets/image 4.png";
+import Language from "../../assets/Globe.png"
+import BarIcon from "../../assets/Minus.png"
+import UserIcon from "../../assets/User.png"
+import MenuIcon from "../../assets/Menu.png"   
 
 export default function Header() {
     return (
         <HeaderContainer>
-            <img src={logo} alt="Logo" />
-            <h4>Header</h4>
+            <img src={logo} alt="Logo" /> 
+            <button onClick={() => { 
+                window.location.href = "/";
+            }}>
+                <img src={helpIcon} alt="Logo" />
+                Ajuda
+            </button>
+
+            <button onClick={() => {
+                window.location.href = "/";
+            }}>
+                <img src={WhatsappIcon} alt="Logo" />
+                Fale Conosco
+            </button>
+
+            <HeaderDiv>
+            <button onClick={() => {
+                window.location.href = "/";
+            }}>
+                <img src={Language} alt="Globo" />
+                PT-BR
+            </button>
+            <img src={BarIcon} alt="" />
+
+            <button onClick={() => {
+                window.location.href = "/";
+            }}>
+                <img src={UserIcon} alt="Globo" />
+                Login
+            </button>
+
+            <button onClick={() => {
+                window.location.href = "/";
+            }}>
+                <img src={MenuIcon} alt="Globo" />
+            </button>
+            </HeaderDiv>
+
+            
         </HeaderContainer>
+    );
+}
+
+function HelpButton() {
+    return (
+        <button onClick={() => {
+            window.location.href = "/";
+        }}>
+            <img src={helpIcon} alt="Logo" />
+        </button>
     );
 }
--- src/components/Header/styles.jsx
+++ src/components/Header/styles.jsx
@@ -1,16 +1,63 @@
 import styled from 'styled-components';
 
 export const HeaderContainer = styled.div`
-position: fixed;
+    position: fixed;
     top: 0;
     left: 0;
     right: 0;
     background-color: #000;
     color: #fff;
-    padding: 0.10rem;
-    text-align: center;
+    padding: 0.5rem 1rem;
     display: flex;
-    justify-content: space-between;
+    justify-content: flex-start;
     align-items: center;
     gap: 1rem;
+    min-height: 68px;
+    max-height: 65px;
+    z-index: 1000;
+
+    > img:first-of-type {
+        height: 135px;
+        width: auto;
+        object-fit: contain;
+        margin-top: 0;
+    }
+
+    button {
+        background: transparent;
+        border: none;
+        color: #fff;
+        cursor: pointer;
+        display: flex;
+        align-items: center;
+        gap: 0.5rem;
+        padding: 0.5rem;
+        font-size: 1rem;
+        
+        &:hover {
+            background: transparent;
+        }
+        
+        &:focus {
+            outline: none;
+            background: transparent;
+        }
+        
+        &:active {
+            background: transparent;
+        }
+        
+        img {
+            height: 20px;
+            width: auto;
+        }
+    }
+
+`;
+
+export const HeaderDiv = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    margin-left: auto;
 `;
